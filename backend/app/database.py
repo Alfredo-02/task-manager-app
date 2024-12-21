90c137df6a3d406695fc7d4cb6d63c0557782ce7
Alfredo Valparaiso <alfredo.valparaiso09@gmail.com>
2024-12-21 00:04:53 -0600

    Configuración de conexión a la base de datos MySQL con SQLAlchemy

--- backend/app/database.py
+++ backend/app/database.py
@@ -2,15 +2,23 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
+# URL de conexión a la base de datos MySQL
 SQLALCHEMY_DATABASE_URL = "mysql://root:Admin08.@localhost/todo_db"
 
+# Se crea el motor de base de datos utilizando la URL de conexión
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+# Se crea la clase SessionLocal, que maneja la sesión de base de datos
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# Es utilizada para definir los modelos de la base de datos
 Base = declarative_base()
 
+# Función que obtiene una sesión de base de datos
 def get_db():
-    db = SessionLocal()
+    db = SessionLocal() # Inicia la sesión
     try:
-        yield db
+        yield db   # Devuelve la sesión a quien la requiera
     finally:
-        db.close()
+        db.close()  # Asegura que la sesión se cierre después de su uso
+ 
