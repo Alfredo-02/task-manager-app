912cf11da08c7e94aaa7255a92a729b56cac4edd
Alfredo Valparaiso <alfredo.valparaiso09@gmail.com>
2024-12-21 00:20:51 -0600

    Implementacion del repositorio TaskRepository con metodos CRUD

--- backend/app/repositories.py
+++ backend/app/repositories.py
@@ -2,14 +2,19 @@ from sqlalchemy.orm import Session
 from . import models, schemas
 
 class TaskRepository:
+    # Repositorio metodos para interactuar con la base de datos y realizar operaciones CRUD sobre la tabla 'task'
+    # Obtiene todas las tareas de la base de datos
     @staticmethod
     def get_all(db: Session):
         return db.query(models.Task).all()
+    
 
+    # Obtiene una tarea por su ID
     @staticmethod
     def get_by_id(db: Session, task_id: int):
         return db.query(models.Task).filter(models.Task.id == task_id).first()
 
+    # Crea una nueva tarea en la base de datos
     @staticmethod
     def create(db: Session, task: schemas.TaskCreate):
         db_task = models.Task(**task.dict())
@@ -17,7 +22,8 @@ class TaskRepository:
         db.commit()
         db.refresh(db_task)
         return db_task
-
+    
+    # ACtualiza una tarea existente en la base de datos
     @staticmethod
     def update(db: Session, task_id: int, task: schemas.TaskUpdate):
         db_task = TaskRepository.get_by_id(db, task_id)
@@ -28,7 +34,8 @@ class TaskRepository:
             db.commit()
             db.refresh(db_task)
         return db_task
-
+    
+    # Elimina una tarea de la base de datos
     @staticmethod
     def delete(db: Session, task_id: int):
         db_task = TaskRepository.get_by_id(db, task_id)
