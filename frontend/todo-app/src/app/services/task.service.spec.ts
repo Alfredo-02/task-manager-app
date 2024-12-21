b11b63e7b233ea880c4422ddabcb51d98f71b47a
Alfredo Valparaiso <alfredo.valparaiso09@gmail.com>
2024-12-21 10:16:32 -0600

    Añade prueba unitaria para verificar la creación del servicio TaskService

--- frontend/todo-app/src/app/services/task.service.spec.ts
+++ frontend/todo-app/src/app/services/task.service.spec.ts
@@ -1,16 +1,16 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing'; // Herramientas para pruebas en Angular
 
-import { TaskService } from './task.service';
+import { TaskService } from './task.service'; // Servicio a probar
 
 describe('TaskService', () => {
-  let service: TaskService;
+  let service: TaskService; // Instancia del servicio a probar
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(TaskService);
+    TestBed.configureTestingModule({}); // Configura el módulo de pruebas
+    service = TestBed.inject(TaskService); // Inyecta el servicio
   });
 
   it('should be created', () => {
-    expect(service).toBeTruthy();
+    expect(service).toBeTruthy(); // Verifica que el servicio se haya creado correctamente
   });
 });
